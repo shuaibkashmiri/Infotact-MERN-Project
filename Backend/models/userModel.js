632d43580ebb395d2656938eca2eda521e05d023
Hazartalam <220101120094@cutm.ac.in>
2025-02-08 16:35:25 +0530

    Added userModel.js in Backend/models

---
+++ Backend/models/userModel.js
@@ -0,0 +1,24 @@
+// Backend/models/userModel.js
+
+const mongoose = require('mongoose');
+
+// User schema definition
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+}, { timestamps: true });
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
